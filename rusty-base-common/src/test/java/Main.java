4a9436a23b45ce327e079accf1b28770c53c9342
GenerixDNS <Lightmicrash@gmail.com>
2022-10-23 03:00:13 +0200

    add test for base common

---
+++ rusty-base-common/src/test/java/Main.java
@@ -0,0 +1,20 @@
+import net.rustmc.cloud.base.common.Rust;
+import net.rustmc.cloud.base.console.ICloudConsole;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 23.10.2022
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        ICloudConsole console = Rust.getInstance().getConsoleFactory().newConsole();
+        console.run();
+        console.send("Hello World!");
+
+    }
+
+}
