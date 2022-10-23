5e04fa4619bfeb87659a8c913caedd91cfa92cf8
GenerixDNS <Lightmicrash@gmail.com>
2022-10-23 12:08:42 +0200

    add cloud api & cloud events

---
+++ rusty-api/src/main/java/net/rustmc/cloud/api/events/CloudConsoleInputEvent.java
@@ -0,0 +1,23 @@
+package net.rustmc.cloud.api.events;
+
+import lombok.Getter;
+import net.rustmc.cloud.base.events.CloudEvent;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 23.10.2022
+ */
+@Getter
+public class CloudConsoleInputEvent extends CloudEvent {
+
+    private final String input;
+    private final String[] arguments;
+
+    public CloudConsoleInputEvent(String input, String[] arguments) {
+        this.input = input;
+        this.arguments = input.split(" ");
+    }
+
+}
