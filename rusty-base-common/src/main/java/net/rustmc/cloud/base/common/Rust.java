1e715f2060662792c15b67824ba639af78d9afeb
GenerixDNS <Lightmicrash@gmail.com>
2022-10-23 12:10:00 +0200

    update common

--- rusty-base-common/src/main/java/net/rustmc/cloud/base/common/Rust.java
+++ rusty-base-common/src/main/java/net/rustmc/cloud/base/common/Rust.java
@@ -3,10 +3,17 @@ package net.rustmc.cloud.base.common;
 import lombok.Getter;
 import net.rustmc.cloud.base.common.configuration.DefaultCloudConfigurationHandler;
 import net.rustmc.cloud.base.common.console.DefaultCloudConsoleFactoryImpl;
+import net.rustmc.cloud.base.common.events.CloudEventPerformerImpl;
+import net.rustmc.cloud.base.common.events.CloudEventRegistryImpl;
 import net.rustmc.cloud.base.common.threads.DefaultCloudThreadPool;
 import net.rustmc.cloud.base.configuration.ICloudConfigurationHandler;
 import net.rustmc.cloud.base.console.ICloudConsoleFactory;
+import net.rustmc.cloud.base.events.ICloudEventPerformer;
+import net.rustmc.cloud.base.events.ICloudEventRegistry;
 import net.rustmc.cloud.base.threads.IThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * This class belongs to the rusty-cloud project
@@ -24,5 +31,8 @@ public final class Rust {
     private final IThreadPool threadPool = new DefaultCloudThreadPool();
     private final String operatingSystem = System.getProperty("os.name");
     private final ICloudConfigurationHandler configurationHandler = new DefaultCloudConfigurationHandler();
+    private final ICloudEventRegistry eventRegistry = new CloudEventRegistryImpl();
+    private final ICloudEventPerformer eventPerformer = new CloudEventPerformerImpl();
+    private final ExecutorService asynchronousExecutor = Executors.newSingleThreadExecutor();
 
 }
