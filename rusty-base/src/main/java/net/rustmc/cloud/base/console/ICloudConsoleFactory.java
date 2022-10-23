b089e4d80524b112641b3820ff579e6d40bbe4e5
GenerixDNS <Lightmicrash@gmail.com>
2022-10-23 02:57:59 +0200

    add console & factory interfaces to base

---
+++ rusty-base/src/main/java/net/rustmc/cloud/base/console/ICloudConsoleFactory.java
@@ -0,0 +1,17 @@
+package net.rustmc.cloud.base.console;
+
+import org.jline.reader.LineReader;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 23.10.2022
+ */
+public interface ICloudConsoleFactory {
+
+    public LineReader getCloudLineReader();
+
+    public ICloudConsole newConsole();
+
+}
