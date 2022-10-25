4ca98deb5a3dc447bea88c888bd77e797021a027
GenerixDNS <Lightmicrash@gmail.com>
2022-10-25 18:12:43 +0200

    add group pool to master

---
+++ rusty-master/src/main/java/net/rustmc/cloud/master/groups/IOfflineGroup.java
@@ -0,0 +1,13 @@
+package net.rustmc.cloud.master.groups;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 25.10.2022
+ */
+public interface IOfflineGroup {
+
+    String getGroupName();
+
+}
---
+++ rusty-master/src/main/java/net/rustmc/cloud/master/groups/IRemoteGroupPool.java
@@ -0,0 +1,10 @@
+package net.rustmc.cloud.master.groups;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 25.10.2022
+ */
+public interface IRemoteGroupPool {
+}
