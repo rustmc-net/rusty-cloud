8cdd305c62747773e82404b0ea3d838a52b5f368
GenerixDNS <Lightmicrash@gmail.com>
2022-10-25 18:54:59 +0200

    update PacketRegistryCluster

---
+++ rusty-base-common/src/main/java/net/rustmc/cloud/base/common/packets/ConstantPacketRegistryCluster.java
@@ -0,0 +1,25 @@
+package net.rustmc.cloud.base.common.packets;
+
+import net.rustmc.cloud.base.common.Rust;
+import net.rustmc.cloud.base.packets.input.handshake.PacketInHandshake;
+import net.rustmc.cloud.base.packets.output.handshake.PacketOutHandshake;
+
+/**
+ * This class belongs to the rusty-cloud project
+ *
+ * @author Alexander Jilge
+ * @since 25.10.2022
+ */
+public class ConstantPacketRegistryCluster {
+
+    @SuppressWarnings("unchecked")
+    public ConstantPacketRegistryCluster() {
+        Rust.getInstance()
+                .getCommunicatePacketPool()
+                .register(
+                        PacketInHandshake.class,
+                        PacketOutHandshake.class
+                );
+    }
+
+}
