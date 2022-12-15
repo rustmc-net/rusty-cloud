e0ececbeb4fad2a4842f4bdda74969fb211bd05a
Schoki287 <Alexander.jilge@web.de>
2022-12-15 17:10:39 +0100

    update group transfer handler

--- rusty-base/src/main/java/net/rustmc/cloud/base/packets/output/transfer/PacketOutGroupTransfer.java
+++ rusty-base/src/main/java/net/rustmc/cloud/base/packets/output/transfer/PacketOutGroupTransfer.java
@@ -1,6 +1,7 @@
 package net.rustmc.cloud.base.packets.output.transfer;
 
 import io.netty.buffer.ByteBuf;
+import lombok.Getter;
 import net.rustmc.cloud.base.communicate.CommunicatePacket;
 import net.rustmc.cloud.base.communicate.PacketIdentifier;
 import net.rustmc.cloud.base.util.ByteBufHelper;
@@ -11,25 +12,30 @@ import net.rustmc.cloud.base.util.ByteBufHelper;
  * @author Alexander Jilge
  * @since 13.12.2022
  */
+@Getter
 @PacketIdentifier(identifier = 'n')
 public class PacketOutGroupTransfer extends CommunicatePacket<PacketOutGroupTransfer> {
 
     private String name;
+    private boolean template;
 
     public PacketOutGroupTransfer() {
     }
 
-    public PacketOutGroupTransfer(String name) {
+    public PacketOutGroupTransfer(String name, boolean template) {
         this.name = name;
+        this.template = template;
     }
 
     @Override
     public void decode(ByteBuf buf) {
         this.name = ByteBufHelper.readString(buf);
+        this.template = buf.readBoolean();
     }
 
     @Override
     public void encode(ByteBuf buf) {
         ByteBufHelper.write(this.name, buf);
+        buf.writeBoolean(this.template);
     }
 }
